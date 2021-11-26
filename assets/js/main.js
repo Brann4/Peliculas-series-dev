23a1099e6ef7a8d08053015822f94f7dcad7f6a4
Brann4 <dennis.brama98@gmail.com>
2021-11-25 19:36:21 -0500

    Agregando cambios de pagina v2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,47 @@ const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
 
+const netflixModal = document.getElementById('netflixModal');
+const hboModal = document.getElementById('hboModal');
+const disneyModal = document.getElementById('disneyModal');
+const amazonPrimeModal = document.getElementById('amazonPrimeModal');
+const starPlusModal = document.getElementById('starPlusModal');
+
+const spotifyModal = document.getElementById('spotifyModal');
+const youtubePremiumModal = document.getElementById('youtubePremiumModal');
+const tidalHifiModal = document.getElementById('tidalHifiModal');
+
+const ultraPlayModal = document.getElementById('ultraPlayModal');
+const movistarPlayModal = document.getElementById('movistarPlayModal');
+
+
+const netflixButton  = document.querySelector('.platform__container_streaming .platform__card:nth-child(1) .platform__button');
+const hboButton  = document.querySelector('.platform__container_streaming .platform__card:nth-child(2) .platform__button');
+const disneyButton= document.querySelector('.platform__container_streaming .platform__card:nth-child(3) .platform__button');
+const amazonPrimeButton = document.querySelector('.platform__container_streaming .platform__card:nth-child(4) .platform__button');
+const starPlusButton = document.querySelector('.platform__container_streaming .platform__card:nth-child(5) .platform__button');
+
+const spotifyButton = document.querySelector('.spotify_button');
+const youtubePremiumButton = document.querySelector('.youtube_premium_button');
+const tidalHifButton = document.querySelector('.tida_hifi_button');
+
+const movistarPlayButton = document.querySelector('.movistar_play_button');
+const ultraPlayButton = document.querySelector('.ultra_play_button');
+
+
+const closeNetflixModal = document.querySelector('#netflixModal .closeBtn');
+const closeHboModal = document.querySelector('#hboModal .closeBtn');
+const closeDisneyModal = document.querySelector('#disneyModal .closeBtn');
+const closeAmazonPrimeModal = document.querySelector('#amazonPrimeModal .closeBtn');
+const closeStarPlusModal = document.querySelector('#starPlusModal .closeBtn');
+
+const closeSpotifyModal = document.querySelector('#spotifyModal .closeBtn');
+const closeYoutubePremiumModal = document.querySelector('#youtubePremiumModal .closeBtn');
+const closeTidiHifiModal = document.querySelector('#tidalHifiModal .closeBtn');
+
+const closeUltraPlayModal = document.querySelector('#ultraPlayModal .closeBtn');
+const closeMovistaPlayModal = document.querySelector('#movistarPlayModal .closeBtn');
+
 /*===== MENU SHOW =====*/
 /* Validate if navToggle exist in DOM*/
 if(navToggle){
@@ -39,35 +80,147 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*==================== SWIPER DISCOVER ====================*/
-let swiper = new Swiper(".plan__container", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    loop: true,
-    spaceBetween: 32,
-    coverflowEffect: {
-        rotate: 0,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
+
+/*==================== MODAL PLANS TRANSITION ====================*/
+
+netflixButton.addEventListener('click', (e) => {
+    netflixModal.style.display = (netflixModal.classList.contains('display_none') ? 'block' : '');
+});
+closeNetflixModal.addEventListener('click', () => {
+    netflixModal.style.display = (netflixModal.classList.contains('display_block') ? 'none' : '');
 });
 
 
+hboButton.addEventListener('click', () => {
+    hboModal.style.display = (hboModal.classList.contains('display_none') ? 'block' : '');
+});
+closeHboModal.addEventListener('click', () => {
+    hboModal.style.display = (hboModal.classList.contains('display_block') ? 'none' : '');
+});
+
+
+disneyButton.addEventListener('click', () => {
+    disneyModal.style.display = (disneyModal.classList.contains('display_none') ? 'block' : '');
+});
+closeDisneyModal.addEventListener('click', () => {
+    disneyModal.style.display = (disneyModal.classList.contains('display_block') ? 'none' : '');
+});
+
+amazonPrimeButton.addEventListener('click', () => {
+    amazonPrimeModal.style.display = (amazonPrimeModal.classList.contains('display_none') ? 'block' : '');
+});
+closeAmazonPrimeModal.addEventListener('click', () => {
+    amazonPrimeModal.style.display = (amazonPrimeModal.classList.contains('display_block') ? 'none' : '');
+});
+
+starPlusButton.addEventListener('click', () => {
+    starPlusModal.style.display = (starPlusModal.classList.contains('display_none') ? 'block' : '');
+});
+closeStarPlusModal.addEventListener('click', () => {
+    starPlusModal.style.display = (starPlusModal.classList.contains('display_block') ? 'none' : '');
+});
+
+
+
+spotifyButton.addEventListener('click', () => {
+    spotifyModal.style.display = (spotifyModal.classList.contains('display_none') ? 'block' : '');
+});
+closeSpotifyModal.addEventListener('click', () => {
+    spotifyModal.style.display = (spotifyModal.classList.contains('display_block') ? 'none' : '');
+});
+
+youtubePremiumButton.addEventListener('click', () => {
+    youtubePremiumModal.style.display = (youtubePremiumModal.classList.contains('display_none') ? 'block' : '');
+});
+closeYoutubePremiumModal.addEventListener('click', () => {
+    youtubePremiumModal.style.display = (youtubePremiumModal.classList.contains('display_block') ? 'none' : '');
+});
+
+tidalHifButton.addEventListener('click', () => {
+    tidalHifiModal.style.display = (tidalHifiModal.classList.contains('display_none') ? 'block' : '');
+});
+closeTidiHifiModal.addEventListener('click', () => {
+    tidalHifiModal.style.display = (tidalHifiModal.classList.contains('display_block') ? 'none' : '');
+});
+
+
+
+
+movistarPlayButton.addEventListener('click', () => {
+    movistarPlayModal.style.display = (movistarPlayModal.classList.contains('display_none') ? 'block' : '');
+});
+closeMovistaPlayModal.addEventListener('click', () => {
+    movistarPlayModal.style.display = (movistarPlayModal.classList.contains('display_block') ? 'none' : '');
+});
+
+ultraPlayButton.addEventListener('click', () => {
+    ultraPlayModal.style.display = (ultraPlayModal.classList.contains('display_none') ? 'block' : '');
+});
+closeUltraPlayModal.addEventListener('click', () => {
+    ultraPlayModal.style.display = (ultraPlayModal.classList.contains('display_block') ? 'none' : '');
+});
+
+
+
+window.addEventListener('click', clickOutside);
+
+
+function clickOutside(e){
+    switch (e.target) {
+        case netflixModal:
+            netflixModal.style.display = 'none';
+            break;
+        case hboModal:
+            hboModal.style.display = 'none';
+            break;
+        case disneyModal:
+            disneyModal.style.display = 'none';
+            break;
+        case amazonPrimeModal:
+            amazonPrimeModal.style.display = 'none';
+            break;
+        case starPlusModal:
+            starPlusModal.style.display = 'none';
+            break;
+        
+        case spotifyModal:
+            spotifyModal.style.display = 'none';
+            break;
+        case youtubePremiumModal:
+            youtubePremiumModal.style.display = 'none';
+            break;
+        case tidalHifiModal:
+            tidalHifiModal.style.display = 'none';
+            break;
+
+        case movistarPlayModal:
+            movistarPlayModal.style.display = 'none';
+            break;
+        case ultraPlayModal:
+            ultraPlayModal.style.display = 'none';
+                break;
+        default:
+            break;
+    }
+}
+
 /*==================== SCROLL REVEAL ANIMATION ====================*/
+
 const sr = ScrollReveal({
     distance: '60px',
     duration: 2800,
     // reset: true,
 })
 
+sr.reveal(`.platform__card`, {
+    origin: 'top',
+    interval: 700,
+});
+
 sr.reveal(`.home__data,
-           .plan__card,
            .home__social`,{
     origin: 'left',
-    interval: 1000,
+    interval: 700,
 });
 
 sr.reveal(`.home__info` ,{ origin: 'right' , interval: 500});
@@ -76,7 +229,5 @@ sr.reveal(`.whatsappBtn,
            .home__social,
            .footer__data`,{
     origin: 'top',
-    interval: 1000,
+    interval: 700,
 });
-
-
